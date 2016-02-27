3eefeb78a76a8d6f291f5b1babf6fef7c115d632
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-02-27 15:55:06 +0100

    Add system controller for login and logout actions

---
+++ src/Controller/SystemController.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link      http://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+
+/**
+ * SystemController
+ */
+class SystemController extends AppController {
+
+    /**
+     * 
+     * @return type
+     */
+    public function login() {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error('Your username or password is incorrect.');
+        }
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    public function logout() {
+        $this->Flash->success('You are now logged out.');
+        return $this->redirect($this->Auth->logout());
+    }
+
+}
